d6249e426f87d616cbada9bc10a466676b886048
DataXplorerFY <farhanyounas5204@gmail.com>
2023-09-09 22:28:18 +0500

    Question 24 completed

--- TypeScript Assignment/Assignment 7 Advanced level/Assignment_7.js
+++ TypeScript Assignment/Assignment 7 Advanced level/Assignment_7.js
@@ -181,14 +181,166 @@ console.log("Sidra cannot join us in the party");
 console.log("We found a bigger table so we will invite more mehmaan");
 // 17(a). Start with your program from Exercise 16. Add a new line that prints a message saying that you can invite only two people for dinner.
 console.log("you can invite only two people for dinner.");
-// Aroojs code 
-var dinnerGuests = ["ali", "arooj", "hassan", "zoya", "ayesha"];
-// Add a new guest to the middle of the array
-dinnerGuests.splice(Math.floor(dinnerGuests.length / 2), 0, "amna");
-// Add a new guest to the end of the array
-dinnerGuests.push("arfa");
-// Print updated invitations
-for (var i = 0; i < dinnerGuests.length; i++) {
-    var invitation = "Dear " + dinnerGuests[i] + ", you are invite.";
-    console.log(invitation);
+// 17(b). Remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+var Guest1 = ["Javeria", "Talha", "Hannan", "sidra"];
+while (Guest1.length > 2) {
+    var Final = Guest1.pop();
+    console.log("you’re sorry you can’t invite them to dinner.", Final);
 }
+// 17(c). Print a message to each of the two people still on your list, letting them know they’re still invited.
+for (let i = 0; i < Guest1.length; i++) {
+    console.log(Guest1[i], "You are still invited");
+}
+// 17(d). Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+for (let i = 0; i <= Guest1.length; i++) {
+    Guest1.pop();
+}
+console.log(Guest1);
+// 18. Seeing the World: Think of at least five places in the world you’d like to visit
+// 18(a) Store the locations in a array. Make sure the array is not in alphabetical order.
+var favLocations = ["a", "e", "t", "c", "f"];
+// 18(b). Print your array in its original order.
+console.log("Array in its origional order is: ", favLocations);
+// 18(c). Print your array in alphabetical order without modifying the actual list.
+var copyArray = [...favLocations]; // it will make an copy of array favlocations
+copyArray.sort();
+console.log(copyArray);
+// 18(d). Show that your array is still in its original order by printing it.
+console.log(favLocations);
+// 18(e). Print your array in reverse alphabetical order without changing the order of the original list.
+var copyArray1 = [...favLocations]; // it will make an copy of array favlocations
+copyArray1.reverse();
+console.log(copyArray1);
+// 18(f). Show that your array is still in its original order by printing it again
+console.log("18(f) origional", favLocations);
+// 18(g). Reverse the order of your list. Print the array to show that its order has changed.
+var copyArray1 = [...favLocations]; // it will make an copy of array favlocations
+copyArray1.reverse();
+console.log(copyArray1);
+// 18(h). Reverse the order of your list again. Print the list to show it’s back to its original order.
+var copyArray1 = [...favLocations]; // it will make an copy of array favlocations
+copyArray1.sort();
+console.log(copyArray1);
+// 18(i). Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+var copyArray1 = [...favLocations]; // it will make an copy of array favlocations
+copyArray1.sort();
+console.log(copyArray1);
+// 18(j). Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+var copyArray1 = [...favLocations]; // it will make an copy of array favlocations
+copyArray1.reverse();
+console.log(copyArray1);
+// 19. Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+var Dinner = ["Ali", "fatima", "Ahmed"];
+var Maxpeople = 0;
+for (let i = 0; i < Dinner.length; i++) {
+    Maxpeople = Maxpeople + i;
+}
+console.log("Totall nuber of people you invited are ", Maxpeople);
+// 20. Think of something you could store in a array. For example, you could make a list
+// of mountains, rivers, countries, cities, languages, or anything else you’d like. Write
+// a program that creates a list containing these items.
+var Arr = ["Python", "Farhan", "Nokia", "Open Ai", "Meta"];
+console.log("List of items");
+console.log(".............");
+var Arr_1 = Arr.map((items, index) => {
+    console.log((index + 1) + ". " + items);
+});
+// 21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var car = {
+    model: 2020,
+    name: "BMW",
+    color: "Black"
+};
+// 22. Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program
+var ErrorIndex = ["Farhan", "Leo", "AI", "Specialist"];
+ErrorIndex.splice(1, 0, "DataScience");
+console.log(ErrorIndex);
+console.log(ErrorIndex[10]); // it will cause an error
+// 23. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// a. Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// Example 1 test for equality
+let num = 42;
+console.log("Is number == 42? I predict True.");
+console.log(num == 42);
+// Example 2  test for enequality
+let fruit = "banana";
+console.log("is fruit is not equal to apple? i pridict the true");
+console.log(fruit != "Apple");
+// Example 3  Numeric comparison
+let num1 = 20;
+console.log("is number<30? i pridecit the true");
+console.log(num1 < 30);
+// Example 4  weekend test
+let day = "sunday";
+console.log("is it sunday? i pridict true");
+console.log(day == "sunday");
+// Example 5  weekend test
+let year = "September";
+console.log("is it September? i pridict true");
+console.log(year == "September");
+// Example 6  logical
+let isweekend = true;
+let hasPlane = false;
+console.log("today i have a plan i pridict false");
+console.log(isweekend && hasPlane);
+// Example 7  logical
+let isRainyDay = true;
+console.log("it is raining ? i pridict false ");
+console.log(!isRainyDay);
+// Example 8  logical
+let isFeePaid = false;
+console.log(" fee is paid ? i pridict false ");
+console.log(isFeePaid);
+// Example 9 logical operators
+let isNameFarhan = true;
+let isAge26 = true;
+console.log("my age is 26? i pridict false");
+console.log(!isAge26);
+// Example 10 logical operators
+let uni = true;
+console.log("i didnt go to uni i prodict false");
+console.log(!uni);
+// 24. More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+// a. Tests for equality and inequality with strings
+// Equality
+let equality = "Leo";
+console.log("is leo==leo? i pridict true");
+console.log(equality == "Leo");
+// inequality
+let equality1 = "Leo";
+console.log("is leo==leo? i pridict true");
+console.log(equality1 == "leo");
+// b. Tests using the lower case function
+let lowerCase = "farhan";
+let isLowerCase = false;
+console.log("it is lower case? i predict true");
+console.log(lowerCase.toLowerCase() == "farhan");
+// c. Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+let a = 10;
+let b = 20;
+console.log("start", a == b);
+console.log(a != b);
+console.log(a >= b && a <= b);
+console.log(a >= b || a <= b);
+console.log(a <= b);
+console.log("............");
+// d. Tests using "and" and "or" operators
+let c = 20;
+let d = 30;
+console.log(c == d && c < d);
+console.log(c == d || c < d);
+console.log("*****");
+// e. Test whether an item is in a array
+var arr2 = ["Chocolate", "Sandwich", "Pizza", "burger"];
+if (arr2.includes("PIzza")) {
+    console.log("Chocolate");
+}
+else {
+    console.log("item not include");
+}
+// for(let item of arr2){
+//     if(item=="pizza"){
+//         console.log("pizza is in the Array",true);
+//     }
+// }
+// f. Test whether an item is not in a array
--- TypeScript Assignment/Assignment 7 Advanced level/Assignment_7.ts
+++ TypeScript Assignment/Assignment 7 Advanced level/Assignment_7.ts
@@ -230,5 +230,222 @@ console.log("We found a bigger table so we will invite more mehmaan");
 
 console.log("you can invite only two people for dinner.");
 
+// 17(b). Remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+
+var Guest1:string[]=["Javeria","Talha","Hannan","sidra"];
+while(Guest1.length>2){
+    var Final=Guest1.pop()
+    console.log("you’re sorry you can’t invite them to dinner.",Final);
+    
+}
 
 
+
+// 17(c). Print a message to each of the two people still on your list, letting them know they’re still invited.
+for(let i=0;i<Guest1.length;i++){
+    console.log(Guest1[i],"You are still invited");
+    
+}
+// 17(d). Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+for(let i=0;i<=Guest1.length;i++){
+    Guest1.pop()
+    
+}
+console.log(Guest1);
+
+
+
+// 18. Seeing the World: Think of at least five places in the world you’d like to visit
+
+// 18(a) Store the locations in a array. Make sure the array is not in alphabetical order.
+var favLocations:string[]=["a","e","t","c","f"];
+// 18(b). Print your array in its original order.
+console.log("Array in its origional order is: ",favLocations);
+// 18(c). Print your array in alphabetical order without modifying the actual list.
+var copyArray:string[]=[... favLocations];// it will make an copy of array favlocations
+copyArray.sort();
+console.log(copyArray);
+
+// 18(d). Show that your array is still in its original order by printing it.
+console.log(favLocations);
+
+// 18(e). Print your array in reverse alphabetical order without changing the order of the original list.
+var copyArray1:string[]=[... favLocations];// it will make an copy of array favlocations
+copyArray1.reverse();
+console.log(copyArray1);
+
+// 18(f). Show that your array is still in its original order by printing it again
+console.log("18(f) origional",favLocations);
+
+// 18(g). Reverse the order of your list. Print the array to show that its order has changed.
+var copyArray1:string[]=[... favLocations];// it will make an copy of array favlocations
+copyArray1.reverse();
+console.log(copyArray1);
+
+// 18(h). Reverse the order of your list again. Print the list to show it’s back to its original order.
+var copyArray1:string[]=[... favLocations];// it will make an copy of array favlocations
+copyArray1.sort();
+console.log(copyArray1);
+// 18(i). Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+var copyArray1:string[]=[... favLocations];// it will make an copy of array favlocations
+copyArray1.sort();
+console.log(copyArray1);
+
+// 18(j). Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+var copyArray1:string[]=[... favLocations];// it will make an copy of array favlocations
+copyArray1.reverse();
+console.log(copyArray1);
+
+// 19. Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+var Dinner:string[]=["Ali","fatima","Ahmed"]
+var Maxpeople:number=0
+for(let i=0;i<Dinner.length;i++){
+    
+    Maxpeople=Maxpeople+i;
+
+    
+}
+console.log("Totall nuber of people you invited are ",Maxpeople);
+
+// 20. Think of something you could store in a array. For example, you could make a list
+// of mountains, rivers, countries, cities, languages, or anything else you’d like. Write
+// a program that creates a list containing these items.
+
+var Arr:string[]=["Python","Farhan","Nokia","Open Ai","Meta"];
+console.log("List of items");
+console.log(".............");
+var Arr_1=Arr.map((items,index)=>{
+    console.log((index + 1) + ". " + items);  
+
+    
+})
+
+// 21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+var car:{model:number,name:string,color:string}={
+    model:2020,
+    name:"BMW",
+    color:"Black"
+};
+
+// 22. Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program
+
+var ErrorIndex:string[]=["Farhan","Leo","AI","Specialist"];
+ErrorIndex.splice(1,0,"DataScience");
+console.log(ErrorIndex);
+console.log(ErrorIndex[10]); // it will cause an error
+
+// 23. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// a. Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// Example 1 test for equality
+let num:number=42;
+console.log("Is number == 42? I predict True.");
+console.log(num==42);
+// Example 2  test for enequality
+let fruit:string="banana";
+console.log("is fruit is not equal to apple? i pridict the true");
+console.log(fruit!="Apple");
+// Example 3  Numeric comparison
+let num1:number=20;
+console.log("is number<30? i pridecit the true");
+console.log(num1<30);
+
+// Example 4  weekend test
+let day:string="sunday"
+console.log("is it sunday? i pridict true");
+console.log(day=="sunday");
+
+// Example 5  weekend test
+let year:string="September"
+console.log("is it September? i pridict true");
+console.log(year=="September");
+
+
+// Example 6  logical
+let isweekend=true;
+let hasPlane=false;
+console.log("today i have a plan i pridict false");
+console.log(isweekend && hasPlane);
+
+// Example 7  logical
+let isRainyDay=true;
+console.log("it is raining ? i pridict false ");
+console.log(!isRainyDay);
+
+// Example 8  logical
+let isFeePaid=false;
+console.log(" fee is paid ? i pridict false ");
+console.log(isFeePaid);
+
+// Example 9 logical operators
+
+let isNameFarhan=true;
+let isAge26=true;
+console.log("my age is 26? i pridict false");
+console.log(!isAge26);
+
+// Example 10 logical operators
+
+let uni=true;
+console.log("i didnt go to uni i prodict false");
+console.log(!uni);
+
+
+// 24. More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+
+// a. Tests for equality and inequality with strings
+// Equality
+let equality:string="Leo";
+console.log("is leo==leo? i pridict true");
+console.log(equality=="Leo");
+// inequality
+let equality1:string="Leo";
+console.log("is leo==leo? i pridict true");
+console.log(equality1=="leo");
+
+
+
+// b. Tests using the lower case function
+
+let lowerCase:string="farhan";
+let isLowerCase=false;
+console.log("it is lower case? i predict true");
+console.log(lowerCase.toLowerCase()=="farhan");
+
+// c. Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+
+let a:number=10;
+let b:number=20;
+console.log("start",a==b);
+console.log(a!=b);
+console.log(a>=b && a<=b);
+console.log(a>=b || a<=b);
+console.log(a<=b);
+console.log("............");
+
+// d. Tests using "and" and "or" operators
+let c:number=20;
+let d:number=30;
+console.log(c==d && c<d);
+console.log(c==d || c<d);
+console.log("*****");
+
+// e. Test whether an item is in a array
+var arr2:string[]=["Chocolate","Sandwich","Pizza","burger"];
+if(arr2.includes("PIzza")){
+    console.log("Chocolate");
+}else{
+    console.log("item not include");
+    
+}
+
+
+// f. Test whether an item is not in a array
+var arr3:string[]=["Chocolate","Sandwich","Pizza","burger"];
+if(arr3.includes("PIzza")){
+    console.log("Chocolate");
+}else{
+    console.log("item not include");
+    
+}
